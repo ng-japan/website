3819b8f06a288abf570d1a45bbfd13430a29c196
maasa <maasa.giss2@gmail.com>
2018-04-15 02:35:07 +0900

    script反映

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,21 @@ const Speakers = require('../data/speakers.json');
 export class AppComponent {
   guests = Speakers.filter(s => s.tagLine.includes('Angular'));
   speakers = Speakers.filter(s => !s.tagLine.includes('Angular'));
+  scroll () {
+    let current = window.document.documentElement.scrollTop;
+    const about = (window.document.querySelector('#about') as HTMLElement).offsetTop;
+    const diff = about - current;
+    const frameDiff = diff / 60 * 6;
+    const frame = () => {
+      window.requestAnimationFrame(() => {
+        if (current > about) {
+          return;
+        }
+        current = current + frameDiff;
+        window.document.documentElement.scrollTop = window.document.body.scrollTop = current;
+        frame();
+      })
+    };
+    frame();
+  }
 }
