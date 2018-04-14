6766b7cd2a7a2fb6af24aed7e0db089d45e433f5
maasa <maasa.giss2@gmail.com>
2018-04-15 02:17:12 +0900

    speakers from API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+declare function require(x: string): any;
+const Speakers = require('../data/speakers.json');
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  lang = 'ja';
+  guests = Speakers.filter(s => s.tagLine.includes('Angular'));
+  speakers = Speakers.filter(s => !s.tagLine.includes('Angular'));
 }
