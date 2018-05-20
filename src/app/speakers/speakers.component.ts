b9b9322be83cc875c083393cebfcac9bdf64d821
maasa <maasa.giss2@gmail.com>
2018-05-20 23:27:38 +0900

    speakers

--- src/app/speakers/speakers.component.ts
+++ src/app/speakers/speakers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Speaker } from '../../types';
 
 declare function require(x: string): any;
 const Speakers = require('../../data/speakers.json');
@@ -9,8 +10,19 @@ const Speakers = require('../../data/speakers.json');
   styleUrls: ['./speakers.component.css']
 })
 export class SpeakersComponent implements OnInit {
-  guests = Speakers.filter(s => s.isTopSpeaker);
-  speakers = Speakers.filter(s => !s.isTopSpeaker);
+  speakers: Speaker[] =
+    Speakers
+      .sort((a, b) => (a.isTopSpeaker === b.isTopSpeaker) ? 0 : a.isTopSpeaker ? -1 : 1)
+      .map(s => {
+        const twitter = s.links.find(l => l.linkType === 'Twitter')
+        if (!twitter) {
+          return s
+        }
+        return {
+          ...s,
+          twitterId: twitter.url.replace('https://twitter.com/', ''),
+        }
+      })
 
   constructor() { }
 
