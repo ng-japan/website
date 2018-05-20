c822c80b0afd666953e8d377527930f736094ea5
maasa <maasa.giss2@gmail.com>
2018-05-20 21:58:51 +0900

    Add data

--- src/app/sessions/sessions.component.ts
+++ src/app/sessions/sessions.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+declare function require(x: string): any;
+const Speakers = require('../../data/sessions.json');
+const Sessions = require('../../data/sessions.json')[0].sessions;
+
 @Component({
   selector: 'app-sessions',
   templateUrl: './sessions.component.html',
   styleUrls: ['./sessions.component.css']
 })
 export class SessionsComponent implements OnInit {
+  sessionMap = Sessions.reduce((obj, value) => ({ ...obj, [value.id]: value }), {});
+  speakerMap = Speakers.reduce((obj, value) => ({ ...obj, [value.id]: value }), {});
 
   constructor() { }
 
--- src/app/speakers/speakers.component.ts
+++ src/app/speakers/speakers.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+declare function require(x: string): any;
+const Speakers = require('../../data/speakers.json');
+
 @Component({
   selector: 'app-speakers',
   templateUrl: './speakers.component.html',
   styleUrls: ['./speakers.component.css']
 })
 export class SpeakersComponent implements OnInit {
+  guests = Speakers.filter(s => s.isTopSpeaker);
+  speakers = Speakers.filter(s => !s.isTopSpeaker);
 
   constructor() { }
 
