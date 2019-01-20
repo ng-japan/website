b91d00caa8cca2fd8162e3cd32c3d83d520c7207
Suguru Inatomi <suguru.inatomi@gmail.com>
2019-01-20 15:57:57 +0900

    fix

--- src/app/pages/top/top.component.ts
+++ src/app/pages/top/top.component.ts
@@ -1,23 +1,7 @@
 import { Component } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-
-declare function require(x: string): any;
-const Speakers = require('../../../data/speakers.json');
-const Sessions = require('../../../data/sessions.json')[0].sessions;
 
 @Component({
   templateUrl: './top.component.html',
   styleUrls: ['./top.component.scss']
 })
-export class TopComponent {
-  guests = Speakers.filter(s => s.isTopSpeaker);
-  speakers = Speakers.filter(s => !s.isTopSpeaker);
-  sessionMap = Sessions.reduce(
-    (obj, value) => ({ ...obj, [value.id]: value }),
-    {}
-  );
-  speakerMap = Speakers.reduce(
-    (obj, value) => ({ ...obj, [value.id]: value }),
-    {}
-  );
-}
+export class TopComponent {}
