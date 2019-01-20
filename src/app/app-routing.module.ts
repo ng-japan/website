9180c394d2e777fe773800850545cb73ecb188b4
Suguru Inatomi <suguru.inatomi@gmail.com>
2019-01-20 16:26:08 +0900

    reove unused code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { SpeakersComponent } from './speakers/speakers.component';
-import { SessionsComponent } from './sessions/sessions.component';
-
 const routes: Routes = [
-  { path: '', loadChildren: './pages/top/top.module#TopModule' },
-  { path: 'speakers', component: SpeakersComponent }
+  { path: '', loadChildren: './pages/top/top.module#TopModule' }
   // { path: 'sessions', component: SessionsComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,26 +7,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderMenuComponent } from './components/header-menu/header-menu.component';
 import { TitleLogoComponent } from './components/title-logo/title-logo.component';
-import { SessionsComponent } from './sessions/sessions.component';
-import { SpeakerAvatarComponent } from './speaker-avatar/speaker-avatar.component';
-import { SpeakersComponent } from './speakers/speakers.component';
-import { SubHeaderComponent } from './sub-header/sub-header.component';
-import { TimetableCardComponent } from './timetable-card/timetable-card.component';
-import { TimetableComponent } from './timetable/timetable.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SubHeaderComponent,
-    TimetableComponent,
-    TimetableCardComponent,
-    SpeakersComponent,
-    SessionsComponent,
-    SpeakerAvatarComponent,
-    TitleLogoComponent,
-    HeaderMenuComponent
-  ],
+  declarations: [AppComponent, TitleLogoComponent, HeaderMenuComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
