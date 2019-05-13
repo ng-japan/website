b75f92e0a7b3c34313d27bae6fe87daa1370b19f
Suguru Inatomi <suguru.inatomi@gmail.com>
2019-05-13 21:55:29 +0900

    add link for sessions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ const routes: Routes = [];
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      anchorScrolling: 'enabled'
+      anchorScrolling: 'enabled',
+      scrollPositionRestoration: 'top'
     })
   ],
   exports: [RouterModule]
